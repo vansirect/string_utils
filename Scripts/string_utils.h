ef161e75f42d8283e799bdb9e047d2426c99179d
Tz <75564936+vansirect@users.noreply.github.com>
2025-02-14 09:22:38 +0300

    Update string_utils.h

--- Scripts/string_utils.h
+++ Scripts/string_utils.h
@@ -1,6 +1,11 @@
-﻿#ifndef STRING_UTILS
+#ifndef STRING_UTILS
 #define STRING_UTILS
 
+#include <unistd.h>
+#include <stdarg.h>
+int print_str(char *str)
+
+int my_printf(const char* format,...)
 
 char* chardup(const char c);
 unsigned int strlen(const char* c);
